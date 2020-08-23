ba50003f6662bf36daddcb5c2b74f97f1aab8865
yourehilarryous <yourehilarryous@gmail.com>
2020-08-22 20:33:35 -0700

    testing api calls

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,24 @@
-const router = require('express').Router()
+const express = require('express')
+const router = express.Router()
+const axios = require('axios')
 
-router.get('/', (req, res) => {
-    res.render('Objective Secure')
+let spotifyURL = ` 	https://api.spotify.com//v1/playlists/37i9dQZF1DWVFJtzvDHN4L/tracks `
+
+let key = '3dfb23664b73446abdb76ccb50b15c0e'
+
+// const test = async() => {
+// }
+
+// test()
+
+router.get('/', async (req, res) => {
+    try {
+        const result = await axios.get(`${spotifyURL}`)
+        console.log(result)
+        res.send(result)
+    } catch (error) {
+        console.log(error)
+    }
 })
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const axios = require('axios')
 
 const app = express()
 const port = process.env.PORT || 3000
 
-// const routes = require('./routes/index')
-// app.use('/home', routes)
+app.use(express.json())
+
+const routes = require('./routes')
+app.use('/', routes)
+
 
 app.listen(port, () => {
     console.log(`We're in ${port}, lets do this`)
