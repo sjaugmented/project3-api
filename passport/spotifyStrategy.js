8059bd4c9c5be3ec97dc43d06859d8f4a1eb53f8
sjaugmented <sjaugmented@gmail.com>
2020-08-24 10:53:31 -0700

    integrated spotify auth with user model

--- passport/spotifyStrategy.js
+++ passport/spotifyStrategy.js
@@ -1,4 +1,4 @@
-const passport = requestAnimationFrame('passport')
+const passport = require('passport')
 const SpotifyStrategy = require('passport-spotify').Strategy
 const User = require('../models/user')
 
@@ -9,11 +9,44 @@ passport.use(
             callbackURL: 'http://localhost:3001/api/v1/auth/callback/'
         },
         function (accessToken, refreshToken, expires_in, profile, done) {
-            User.findOrCreate({
+            User.findOne({
                 spotifyId: profile.id
             }, function (err, user) {
-                return done(err, user);
+                    if (user) {
+                        user.name = profile.displayName
+                        user.loggedIn = true
+                        user.save(function (err) {
+                            return done(null, user)
+                        })
+                        console.log('>>>>>>>>>>>>>>>>>>>>>>>')
+                        console.log('user:', user)
+                    } else {
+                        // new user!
+                        const newUser = new User({
+                            spotifyId: profile.id,
+                            name: profile.displayName,
+                            loggedIn: true
+                        })
+                        console.log('>>>>>>>>>>>>>>>>>>>>>>');
+                        console.log('newUser:', newUser)
+                        newUser.save(function (err) {
+                            if (err) return done(err)
+                            return done(null, newUser)
+                        })
+                    }                    
             });
         }
     )
 );
+
+// called everytime a user logs in
+passport.serializeUser(function (user, done) {
+    done(null, user._id);
+});
+
+// called with every user's request
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+})
--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
 var cookieParser = require('cookie-parser');
 const mongoose = require('mongoose')
+const passport = require('passport')
 
 
+require('./passport/spotifyStrategy')
 
 
 //set up port
@@ -20,8 +22,36 @@ app.use(express.json())
 app.use(cors()).use(cookieParser())
 
 // middleware - sessions config?
+app.use(session({
+    secret: 'I am mustard',
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+
 app.use('/api/v1', routes.playlists)
-app.use('/api/v1/auth', routes.spotify)
+//app.use('/api/v1/auth', routes.spotify)
+app.get('/api/v1/auth/login', passport.authenticate('spotify', {
+    scope: ['user-read-email', 'user-read-private'],
+    showDialog: true
+}), function (req, res) {
+    // The request will be redirected to spotify for authentication, so this
+    // function will not be called.
+})
+
+app.get(
+    '/api/v1/auth/callback',
+    passport.authenticate('spotify', {
+        failureRedirect: '/api/v1/auth/login'
+    }),
+    function (req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('http://localhost:3000/');
+    }
+)
 
 app.use('/', (req, res) => {
     res.send('Ready')
