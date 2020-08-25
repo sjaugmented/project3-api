c2ce5bcaa9cd6cc28dbab28d3a1c46a13396226b
sjaugmented <sjaugmented@gmail.com>
2020-08-25 14:46:18 -0700

    added accessToken to User mode & verify route

--- models/index.js
+++ models/index.js
@@ -7,13 +7,13 @@ const configOptions = {
     useCreateIndex: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
-};
+}
 
 mongoose.connect(connectionString, configOptions)
     .then(() => console.log(`MongoDB successfully connected to ${connectionString}`))
-    .catch(err => console.log(`MongoDB connection error: ${err}`));
+    .catch(err => console.log(`MongoDB connection error: ${err}`))
 
 module.exports = {
     Playlist: require('./playlist'),
     User: require('./user')
-};
+}
