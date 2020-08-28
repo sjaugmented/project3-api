9c0e5d23610bfa6285b0619353b827274840ad8d
sjaugmented <sjaugmented@gmail.com>
2020-08-28 11:26:21 -0700

    updated post creation to include users

--- controllers/posts.js
+++ controllers/posts.js
@@ -13,17 +13,21 @@ const index = async (req, res) => {
 }
 
 const create = async (req, res) => {
-  console.log('req.body>>>>', req.body.body)
     try {
         const data = await JSON.parse(req.body.body)
         const createdPost = await db.Post.create(data)
+
         const foundPlaylist = await db.Playlist.findById(req.params.id)
-      
-        // const foundPlaylist = await db.Playlist.findById(req.body.playlistId)
         foundPlaylist.posts.push(createdPost)
-        await createdPost.save()
         await foundPlaylist.save()
-        console.log('foundPlaylist>>>>', foundPlaylist)
+        
+        const foundUser = await db.User.findOne({
+            spotifyId: data.userSpotId
+        })
+        foundUser.posts.push(createdPost)
+        await foundUser.save()
+        
+        await createdPost.save()
         await res.json({post: createdPost})
     } catch (error) {
         console.log(error)
