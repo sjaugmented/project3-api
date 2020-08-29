d52b252d9d7455e050c18352d8c8a491bb675679
sjaugmented <sjaugmented@gmail.com>
2020-08-29 12:09:05 -0700

    fixing deploy bugs

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const routes = require('./routes')
 const cors = require('cors')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
-const cookieParser = require('cookie-parser');
 const passport = require('passport')
 
 
@@ -20,7 +19,7 @@ app.use(express.json())
 
 // CORS
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    origin: ['https://spotify-us/herokuapp.com/api/v1'],
     credentials: true,
     optionsSuccessStatus: 200
 }))
@@ -31,7 +30,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     store: new MongoStore({
-        url: process.env.DATABASE_URL || "mongodb://localhost:27017/p3"
+        url: process.env.MONGODB_URI || "mongodb://localhost:27017/p3"
     }),
     cookie: {maxAge: 1000 * 60 * 60 * 24}
 }))
@@ -45,6 +44,6 @@ app.use('/api/v1/posts', routes.posts)
 app.use('/api/v1/users', routes.users)
 app.use('/api/v1/auth', routes.spotify)
 
-app.listen(process.env.PORT || 3001, () => {
+app.listen(process.env.PORT, () => {
     console.log(`We're in ${process.env.PORT}, lets do this`)
 })
