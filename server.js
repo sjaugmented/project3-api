e549817481e7a5ef78f646961b9444666ca65d29
sjaugmented <sjaugmented@gmail.com>
2020-08-29 16:20:32 -0700

    extra cors options

--- server.js
+++ server.js
@@ -24,6 +24,16 @@ app.use(cors({
     optionsSuccessStatus: 200,
 }))
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    next();
+});
+
 // passport & sessions
 app.use(session({
     secret: process.env.SESSION_SECRET,
