6182c954e3329fbff3c5d9feb863025137778e6e
sjaugmented <sjaugmented@gmail.com>
2020-08-30 13:30:18 -0700

    cors

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(cors({
 }))
 // do I need this below?
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "https://spotify-us.herokuapp.com/");
     res.setHeader("Vary", "Origin")
     res.setHeader(
         "Access-Control-Allow-Headers",
