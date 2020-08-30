0bd4fae8cd341ac4b0b7b7aafc2c6b1da5526390
sjaugmented <sjaugmented@gmail.com>
2020-08-30 08:21:43 -0700

    cors

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
         "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Credentials",
         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
