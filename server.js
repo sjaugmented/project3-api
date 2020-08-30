eeb23dc72d9db62551a3c3a5a94bfe3345cf8626
sjaugmented <sjaugmented@gmail.com>
2020-08-30 08:24:37 -0700

    cors

--- server.js
+++ server.js
@@ -25,10 +25,9 @@ app.use(cors({
 }))
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "https://spotify-us.herokuapp.com/");
     res.setHeader(
         "Access-Control-Allow-Headers",
-        "Access-Control-Allow-Credentials",
         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
