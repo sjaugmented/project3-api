6d52956321f8dd2cad5d7c2114ef79006f8f82df
sjaugmented <sjaugmented@gmail.com>
2020-09-03 16:48:57 -0700

    cors reset

--- server.js
+++ server.js
@@ -21,16 +21,16 @@ app.use(cors({
 }))
 
 // do I need this below?
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", 'https://spotify-us.herokuapp.com');
-    // res.setHeader("Vary", "Origin")
-    // res.setHeader(
-    //     "Access-Control-Allow-Headers",
-    //     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    // );
-    // res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    // next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", process.env.LOCAL_CLIENT);
+//     res.setHeader("Vary", "Origin")
+//     res.setHeader(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//     );
+//     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//     next();
+// });
 
 // passport & sessions
 app.use(session({
