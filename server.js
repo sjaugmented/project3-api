f4a277db79be11534165736805309276db7679ce
sjaugmented <sjaugmented@gmail.com>
2020-09-05 23:00:42 -0700

    profile show route is working

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json())
 
 // CORS
 app.use(cors({
-    origin: [process.env.HEROKU_CLIENT],
+    origin: [process.env.LOCAL_CLIENT],
     credentials: true,
     optionsSuccessStatus: 200,
 }))
